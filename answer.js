171f2270015a461f0e0b1ecf4e683f57d6881764
reeganford <rmford1695@gmail.com>
2019-12-06 10:25:31 -0700

    Finished challenge #6

--- answer.js
+++ answer.js
@@ -1,1 +1,11 @@
 // *** YOUR ANSWER BELOW ***
+
+function fibonacci(n) {
+    if(n <= 2){
+        return 1;
+    }else {
+       return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+console.log(fibonacci(50))
